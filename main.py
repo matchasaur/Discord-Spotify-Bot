31be3b1392626f2f7d4e4feec20c4e6d578a4664
Jared.Tan <Jtanu45@gmail.com>
2024-03-04 00:36:19 -0800

    sb!deleteP now deletes playlists from database

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message, utils, CategoryChannel
+from discord import Intents, Client, Message, utils, CategoryChannel, ui, ButtonStyle, Interaction, Embed
+import discord
 from responses import get_response
 from discord.ext import commands
 from api import spotifyInit, create_playlist
@@ -127,6 +128,73 @@ async def createP(ctx, *args):
     result = collection.insert_one(doc)
     print(f"Document inserted with id: {result.inserted_id}")
 
+#delete playlist within text-channel
+@bot.command()
+async def deleteP(ctx):
+    class Menu(ui.View):
+        def __init__(self, channelid: str, guildid: str, ownerid: str):
+            super().__init__()
+            self.value = None
+            self.channelid = channelid
+            self.guildid = guildid
+            self.ownerid = ownerid
+            
+        @ui.button(label='Cancel', style=ButtonStyle.grey)
+        async def option1(self, interaction: Interaction, button: ui.Button):
+            await interaction.response.send_message('Did not delete')
+            self.value = False
+            self.stop()
+            
+        @ui.button(label='Delete', style=ButtonStyle.danger)
+        async def option2(self, interaction: Interaction, button: ui.Button):
+            guild = self.guildid
+            channel = self.channelid
+            owner = self.ownerid
+            name = delete_playlist(channel_id=channel, guild_id=guild, owner_id=owner)
+            await interaction.response.send_message(f'{name} has been deleted')
+            #debug
+            #print('guild: ', guild, '\nchannel: ', channel)
+            self.value = False
+            self.stop()
+            
+    user = ctx.author
+    guild = ctx.guild.id
+    channel = ctx.channel
+    owner_id = user.id
+    channel_id = channel.id
+    
+    #If the author is not the owner of the playlist then return
+    if not check_owner(owner_id=owner_id, channel_id=channel_id):
+        await ctx.reply('You do not have permission to delete this channel')
+        return
+    
+    embed = Embed(color=discord.Color.red())
+    embed.add_field(name='', value='Are you sure you want to order the eggroll?')
+    view = Menu(channelid=channel_id, guildid=guild, ownerid=owner_id)
+    await user.send(embed=embed, view=view)
+
+
+
+#Helper functions
+
+#check if author is owner of playlist/channel
+def check_owner(owner_id: str, channel_id: str):
+    collection = database.playlists_info
+    result = collection.find_one({'ownerid': owner_id, 'channelid': channel_id})
+    return result
+
+def delete_playlist(channel_id: str, guild_id: str, owner_id: str):
+    collection = database.playlists_info
+    doc = collection.find_one({'ownerid': owner_id, 'channelid': channel_id})
+    name = doc.get('name', None)
+    result = collection.delete_one({'ownerid': owner_id, 'channelid': channel_id, 'guildid': guild_id})
+    
+    if result.deleted_count > 0:
+        print(f'Document with guildid: {guild_id}, channelid: {channel_id}, and ownerid: {owner_id} has been deleted')  # Document was found and deleted
+        return name
+    else:
+        print('Document not found')  # Document was not found
+
 # @bot.command()
 # async def create_channel(ctx, channel_name: str):
 #     guild = ctx.guild
@@ -154,7 +222,7 @@ async def createP(ctx, *args):
 
 
 def main() -> None:
-    spotifyInit()
+    #spotifyInit()
     # client.run(token=TOKEN)
     bot.run(token=TOKEN)
     
